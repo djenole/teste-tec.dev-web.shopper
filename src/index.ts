78301578492edcad783e460355e4aa4168a5d777
Djenole da Silva <djenole@gmail.com>
2024-08-28 02:26:47 -0300

    feat: criado configuração básica servidor express e uso de routes

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import userRoutes from './routes/userRoutes';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
